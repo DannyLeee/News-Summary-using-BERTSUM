665e5fd5aeac1be3b2dc6d93e63615958cff8c9a
danny <dannylee94049@gmail.com>
2020-12-11 20:19:57 +0800

    update abstractive inference
    basic inference done

--- get_summary.py
+++ get_summary.py
@@ -13,25 +13,31 @@ def get_summary(mode, encoder, origin_text):
     # preprocess to bert_data
     timestamp("preprocess to bert_data")
     bert_data = get_bert_data(origin_text, LM=LM)
+    bert_data["tgt"] = []
+    if mode == "abs":
+        for i, ids in enumerate(bert_data["src"]):
+            bert_data["src"][i] = ids+1 if ids!=0 else 0
     torch.save([bert_data], "dataset/inference/infer.test.pt")
 
     # feed into model by calling command
+    BERT_DATA_PATH = "./dataset/inference/infer"
+    RESULT_PATH = f"./result/inference/infer_{mode}_{model}"
     timestamp("feed to model")
     if mode == "ext":
-        command = f"python3 ../BertSum/src/train.py -mode test -report_rouge false -bert_data_path ./dataset/inference/infer -visible_gpus 0  -gpu_ranks 0 -world_size 1 -batch_size 3000 -decay_method noam -log_file ./logs/inference -use_interval true -temp_dir ./temp -result_path ./result/inference/infer_{model} -rnn_size 768 -bert_config_path {LM}/config.json -test_from ../BertSum/models/NewsSummary/LCSTS/bert_{model}/model_step_20000.pt -encoder {model}" # TODO: can change model?
+        command = f"python3 /home/danny/BertSum/src/train.py -mode test -report_rouge false -bert_data_path {BERT_DATA_PATH}  -visible_gpus 0  -gpu_ranks 0 -world_size 1 -batch_size 3000 -decay_method noam -log_file ./logs/inference -use_interval true -temp_dir ./temp -result_path {RESULT_PATH} -rnn_size 768 -bert_config_path {LM}/config.json -test_from ../BertSum/models/NewsSummary/LCSTS/bert_{model}/model_step_20000.pt -encoder {model}" # TODO: can change model?
     elif mode == "abs":
-        command = f"python3 " # TODO
+        command = f"python3 /home/B10615023/PreSummWWM/src/train.py -task abs -mode test -report_rouge false -batch_size 3000 -test_batch_size 500 -bert_data_path {BERT_DATA_PATH} -log_file ./logs/inference -sep_optim true -use_interval true -visible_gpus 0 -max_pos 512 -alpha 0.95 -result_path {RESULT_PATH} -min_length 5 -max_length 25 -beam_size 10 -test_from /home/B10615023/PreSummWWM/model/model_step_500000.pt"
     os.system(command)
 
     # get output
     list_of_files = glob.glob('./result/inference/*.candidate') # * means all if need specific format then *.csv
     latest_file = max(list_of_files, key=os.path.getctime)
     with open(latest_file) as fp:
-        return fp.readline()[:-1].split("<q>") # summary
+        return fp.readline()[:-1].replace(' ', '').split("<q>") # summary
 
-mode = "ext"
-encoder = "rnn"
-origin_text = """歡迎回到新聞現場關心國際焦點 
+mode = "abs"
+encoder = "trans"
+origin_text = """歡迎 回到 新聞 現場 關心 國際 焦點 
 以 巴 持續 傳出 流血衝突 
 不過 以色列 星期二 展開 另 一 波 撤軍 行動 退出 先前 占領 的 巴勒斯坦 政經 中心 拉 瑪 拉 
 而 根據 正在 歐洲 的 以色列 外長 表示 他 和 以色列 總理 正在 著手 新 的 中東 和平 計畫 
@@ -54,6 +60,5 @@ origin_text = """歡迎回到新聞現場關心國際焦點
 包括 一 項 停火 協議 以及 以 聯合國 的 議案 為 基礎 和 巴勒斯坦 人 展開 談判 
 在 聯合國 的 決議案 當中 要求 以色列 撤出 一 九 七 六 年 以 阿 戰爭 中 所 占領 的 土地 
 同時 建立 一個 巴勒斯坦 國 
-公視 新聞 陳 秋 玫 編譯 
-"""
+公視 新聞 陳 秋 玫 編譯"""
 print(get_summary(mode, encoder, origin_text))
