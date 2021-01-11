1e7cbaa93d341420a2e29b6ce788f55c7cd8641f
danny <dannylee94049@gmail.com>
2021-01-11 17:48:55 +0800

    add search
    and update flask api

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, make_response
 from get_summary import get_summary as __get_summary
+from search import get_search_results
 from multiprocess import Pool
 import json
 
@@ -15,7 +16,10 @@ def get_summary():
     # summary = __get_summary(mode, decoder, source_text)
     with Pool(2) as pool:
         pool_output = pool.starmap(__get_summary, [("abs", "trans", source_text), ("ext", "trans", source_text)])
-    summary = json.dumps({"abs":pool_output[0], "ext":pool_output[1]}, ensure_ascii=False)    
+    abs_summary, ext_summary = pool_output[:2]
+    search_results = get_search_results(abs_summary)
+    summary = json.dumps({"abs": abs_summary, "ext": ext_summary[0], "highlighted": ext_summary[1], "searchResults": search_results}, ensure_ascii=False)
+    print(summary)
     resp = make_response(summary)
     resp.status_code = 200
     resp.headers['Access-Control-Allow-Origin'] = '*'
